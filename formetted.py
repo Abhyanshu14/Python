3e70cd8ec2a1bc41fb4afadaa160013552d2dad0
Abhyanshu <abhyanshu@Abhyanshus-MacBook-Air.local>
2026-02-05 23:44:16 +0530

    formetted string and some basic

---
+++ formetted.py
@@ -0,0 +1,9 @@
+name="abhyanshu"
+age=20
+print(f"my name is {name} and my age is{age}")
+h="hello world"
+print("hello \n world")
+print("hello \t world")
+print("j"*3)
+print("hello \\ world")
+print("hello hii\" world")

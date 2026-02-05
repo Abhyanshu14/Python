9abf1bff4e79594b98147edb39de4ce4c8a847c7
Abhyanshu <abhyanshu@Abhyanshus-MacBook-Air.local>
2026-02-05 23:41:52 +0530

    this is the basic code of string

---
+++ string.py
@@ -0,0 +1,10 @@
+a="hello "
+b="world"
+print(a+b)
+print(len(a)) #len gives the length of string
+print(a[3])
+# #********************
+name=input("enter your name")
+print(name[0])
+print(name[len(name)-1])
+print(len(name))

ba921eca188a78da959d3be44c37ede4c5ca3ad9
Abhyanshu14 <abhyanshumishra14@gmail.com>
2026-02-05 21:49:52 +0530

    This is the some basic code of python

---
+++ second.py
@@ -0,0 +1,21 @@
+print(("hello"+"\n")*10)
+#***********************************
+x=5
+y="5.5"
+print(id(x))
+print(id(y))
+#***************************************
+print("value is:)",x)
+print("value is:) ",y)
+#****************************************
+#input:- always takes string we must have to typecast it
+z=input("enter your name")
+print(z)
+z=int(input("enter your age"))
+print(z)
+#********************************************************
+#round is used to round of the floating values
+y=3.2355
+z=round(y,1)
+print(z)
+
